a36d137b4b474c9fa9527a1fed1006ade08b1543
Zahari Tilev <zahari@addonsforgapps.com>
2022-03-13 23:48:37 +0200

    Updating the remark plugins for YouTube and Awesome Table to handle multiple embeds in the same markdown node

--- src/remark/convertYoutubeEmbeds.js
+++ src/remark/convertYoutubeEmbeds.js
@@ -4,18 +4,12 @@ const plugin = (options) => {
   const transformer = async (ast) => {
     visit(ast, 'jsx', (node) => {
         if (node.value.includes("www.youtube.com/watch")) {
-            // console.log(node.value);
-            let array1;
-            let rx = /<object data="https:\/\/www\.youtube\.com\/watch\?v=[a-zA-Z0-9-_]{11}">\s*<p \/>\s*<\/object>/i;            
-            array1 = rx.exec(node.value);
-            // console.log(array1);
+          const regexp = /<object data="https:\/\/www\.youtube\.com\/watch\?v=[a-zA-Z0-9-_]{11}">\s*<p \/>\s*<\/object>/g;
+          const matches = [...node.value.matchAll(regexp)];
 
-            for (i = 0; i < array1.length; i++ ) {
-                let YouTubeID = array1[i].substring(46, 57);
-                // console.log(YouTubeID);
-                // console.log(array1[i]);
-
-                const iframe = `
+          for (match of matches) {
+            let YouTubeID = match[0].substring(46, 57);
+            const iframe = `
                   <iframe 
                     class="embed-responsive-item" 
                     src="https://www.youtube.com/embed/${YouTubeID}" 
@@ -24,10 +18,8 @@ const plugin = (options) => {
                     frameborder="0" 
                     allowfullscreen="">
                   </iframe>
-                `;
-                
-                node.value = node.value.replace(array1[i], iframe);
-                // console.log(node.value);
+            `;
+            node.value = node.value.replace(match[0], iframe);
           }
         }
     });
--- src/remark/createAwesomeTableEmbeds.js
+++ src/remark/createAwesomeTableEmbeds.js
@@ -5,25 +5,50 @@ const plugin = (options) => {
     visit(ast, 'jsx', (node) => {
         if (node.value.includes('type="AwesomeTable"')) {
             // console.log(node.value);
-            let array1;
-            let rx = /<object xmlns="http:\/\/www\.w3\.org\/1999\/xhtml" data="[a-zA-Z0-9\-\_]{20}" type="AwesomeTable\"> <\/object>/i;
-            array1 = rx.exec(node.value);
-            for (i = 0; i < array1.length; i++ ) {
-                let AwesomeTableID = array1[i].substring(51, 71);
-                // console.log(AwesomeTableID);
-                // console.log(array1[i]);
 
-                const iframe = `
-                    <iframe 
-                        referrerpolicy="no-referrer-when-downgrade" 
-                        height="600px" 
-                        width="100%" 
-                        style={{border: "none",}} 
-                        src="https://view-awesome-table.com/${AwesomeTableID}/view">
-                    </iframe>
-                `;
-                node.value = node.value.replace(array1[i], iframe);
-                // console.log(node.value);
+            // A regex that matches <object> with and without height attribute specified
+            // Example 1:
+            // <object xmlns="http://www.w3.org/1999/xhtml" data="-Mx0giR4SuKQLt065uhz" height="960px" type="AwesomeTable"> </object> 
+            //
+            // Example 2:
+            // <object xmlns="http://www.w3.org/1999/xhtml" data="-Mx0giR4SuKQLt065uhz" type="AwesomeTable"> </object> 
+            //
+            const regexp = /<object xmlns="http:\/\/www\.w3\.org\/1999\/xhtml" data="[a-zA-Z0-9\-\_]{20}"( height="[0-9]{3,4}(px)?")? type="AwesomeTable\"> <\/object>/g;
+            
+            // matchAll() creates an array of the matched strings
+            // Unpack the strings into the 'matches' constant
+            const matches = [...node.value.matchAll(regexp)];
+            // console.log(matches);
+            for (match of matches) {
+                // Extract the Awesome Table app ID
+                let AwesomeTableID = match[0].substring(51, 71);
+                // If height is specified, extract it, and add it to the iframe
+                if ( match[0].includes("height=") ) {
+                    let AwesomeTableHeight = match[1];
+                    console.log(AwesomeTableHeight);
+                    const iframe = `
+                        <iframe 
+                            referrerpolicy="no-referrer-when-downgrade" 
+                            ${AwesomeTableHeight}
+                            width="100%" 
+                            style={{border: "none",}} 
+                            src="https://view-awesome-table.com/${AwesomeTableID}/view">
+                        </iframe>
+                    `;
+                    node.value = node.value.replace(match[0], iframe);
+                // If height is not specified, apply the defaul 600px
+                } else {
+                    const iframe = `
+                        <iframe 
+                            referrerpolicy="no-referrer-when-downgrade" 
+                            height="600px" 
+                            width="100%" 
+                            style={{border: "none",}} 
+                            src="https://view-awesome-table.com/${AwesomeTableID}/view">
+                        </iframe>
+                    `;
+                    node.value = node.value.replace(match[0], iframe);
+                }
             }
         }
     });
