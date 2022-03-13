a9341cc6f930070b5a4aa3fd15ebd698e38f61ac
Zahari Tilev <zahari@addonsforgapps.com>
2022-03-13 19:30:34 +0200

    Re-created the YouTube remark plugin to pattern-match object tags with regex

--- src/remark/convertYoutubeEmbeds.js
+++ src/remark/convertYoutubeEmbeds.js
@@ -4,31 +4,31 @@ const plugin = (options) => {
   const transformer = async (ast) => {
     visit(ast, 'jsx', (node) => {
         if (node.value.includes("www.youtube.com/watch")) {
-            const objectStart = node.value.indexOf("<object");
-            // console.log(objectStart);
-            const objectEnd = node.value.indexOf("</object>") + 9;
-            // console.log(objectEnd);
-            const objSubstr = node.value.substring(objectStart, objectEnd);
-            // console.log(objSubstr);
-            const youtubeIdStart = objSubstr.indexOf("watch") + 8;
-            const youtubeIdEnd = objSubstr.indexOf('">');
-            const youtubeID = objSubstr.substring(youtubeIdStart, youtubeIdEnd);
-            // console.log(youtubeID);
-
-            const iframe = `
-            <iframe 
-              class="embed-responsive-item" 
-              src="https://www.youtube.com/embed/${youtubeID}" 
-              width="560" 
-              height="315" 
-              frameborder="0" 
-              allowfullscreen="">
-            </iframe>
-            `;
-            // console.log(objSubstr);
-            // console.log(iframe);
-            node.value = node.value.replace(objSubstr, iframe);
             // console.log(node.value);
+            let array1;
+            let rx = /<object data="https:\/\/www\.youtube\.com\/watch\?v=[a-zA-Z0-9-_]{11}">\s*<p \/>\s*<\/object>/i;            
+            array1 = rx.exec(node.value);
+            // console.log(array1);
+
+            for (i = 0; i < array1.length; i++ ) {
+                let YouTubeID = array1[i].substring(46, 57);
+                // console.log(YouTubeID);
+                // console.log(array1[i]);
+
+                const iframe = `
+                  <iframe 
+                    class="embed-responsive-item" 
+                    src="https://www.youtube.com/embed/${YouTubeID}" 
+                    width="560" 
+                    height="315" 
+                    frameborder="0" 
+                    allowfullscreen="">
+                  </iframe>
+                `;
+                
+                node.value = node.value.replace(array1[i], iframe);
+                // console.log(node.value);
+          }
         }
     });
   };
